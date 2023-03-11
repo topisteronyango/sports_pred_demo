e3d1ba126d54ff4236bf300f5cba955223d17a96
t-onyango <t.onyango@alustudent.com>
2023-03-11 11:35:32 +0200

    ML app

--- app.py
+++ app.py
@@ -16,14 +16,15 @@ def predict():
     For rendering results on HTML GUI
     '''
     int_features = [int(x) for x in request.form.values()]
-    final_features = [np.array(int_features)]
+    final_features = [np.array(int_features) / 100]
     prediction = model.predict(final_features)
 
     output = round(prediction[0], 46)
 
-    return render_template('index.html', prediction_text='Players rating prediction is: {}'.format(output))
+    return render_template('index.html', prediction_text='Model perfomance is: {}'.format(output))
 
 
 if __name__ == "__main__":
-    # app.run(debug=True)
-    app.run(host='0.0.0.0', port=80)
+    app.run(debug=True, host='0.0.0.0', port=80)
+    # app.run(host='0.0.0.0', port=80)
+    # log.Fatal(http.ListenAndServe(":" + os.Getenv("PORT"), router))
